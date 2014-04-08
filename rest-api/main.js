db6d956093e0e4f679ae79bbe39b9f0be1f86ebb
meilens <m.eilens@gmx.de>
2014-04-08 17:53:05 +0200

    changed response status

--- rest-api/main.js
+++ rest-api/main.js
@@ -22,6 +22,7 @@ var Event = db.define('event', {
     amount: { type: "number" }
  });
 
+// synchronize the model definition to the database (only first creation, no modifications supported)
 Event.sync();
 
  app.configure(function () {
@@ -36,21 +37,24 @@ app.get('/hello.txt', function(req, res){
 app.post('/rest/event', function(req, res) {
     var event = req.body;
     console.log('Adding Event: ' + JSON.stringify(event));
-    res.send(JSON.stringify({id: 'to be done', message: "ok"}));
 
     var newEvent = new Event({
         "type": event.type,
         "ean": event.ean,
-        "created": event.date,
+        "created": new Date(event.date),
         amount: event.amount
     });
 
     newEvent.save(function (err, createdEvent) {
         if (!err) {
             console.log("Saved! ID=" + createdEvent.id);
+            res.statusCode = 201;
+            res.send(JSON.stringify({id: createdEvent.id, message: "ok"}));
         } else {
             console.log("Something went wrong...");
             console.dir(err);
+            res.statusCode = 400;
+            res.send(JSON.stringify({message: "nok"}));
         }
     });
 });
