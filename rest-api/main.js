9f850fb05b2435d1dc400b6d1ea590d89d89d807
meilens <m.eilens@gmx.de>
2014-04-08 17:17:19 +0200

    WIP: we can insert data into mysql - yippie

--- rest-api/main.js
+++ rest-api/main.js
@@ -1,9 +1,30 @@
 'use strict';
 
 var express = require('express');
+var mysql =  require('mysql');
+var orm = require('orm');
 var app = express();
 
-app.configure(function () {
+var db = orm.connect("mysql://shoppinguser:test1234@127.0.0.1/shopping", function (success, db) {
+    if (!success) {
+        console.log("Could not connect to database!");
+        return;
+    } else {
+        console.log("Connected to database !!!")
+        return;
+    }
+});
+
+var Event = db.define('event', {
+    type: { type: "text" },
+    ean: { type: "number" },
+    created: { type: "date" },
+    amount: { type: "number" }
+ });
+
+Event.sync();
+
+ app.configure(function () {
     app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
     app.use(express.bodyParser());
 });
@@ -16,6 +37,22 @@ app.post('/rest/event', function(req, res) {
     var event = req.body;
     console.log('Adding Event: ' + JSON.stringify(event));
     res.send(JSON.stringify({id: 'to be done', message: "ok"}));
+
+    var newEvent = new Event({
+        "type": event.type,
+        "ean": event.ean,
+        "created": event.date,
+        amount: event.amount
+    });
+
+    newEvent.save(function (err, createdEvent) {
+        if (!err) {
+            console.log("Saved! ID=" + createdEvent.id);
+        } else {
+            console.log("Something went wrong...");
+            console.dir(err);
+        }
+    });
 });
 
 
