43f7bc39e0a36fb8c4c00ecb1e3d0962e32bc63b
Markus Lauer <markus.lauer@web.de>
2014-04-08 14:55:18 +0200

    Post method für /rest/event als Dummy

--- rest-api/main.js
+++ rest-api/main.js
@@ -3,9 +3,22 @@
 var express = require('express');
 var app = express();
 
+app.configure(function () {
+    app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
+    app.use(express.bodyParser());
+});
+
 app.get('/hello.txt', function(req, res){
     res.send('Hello World');
 });
+
+app.post('/rest/event', function(req, res) {
+    var event = req.body;
+    console.log('Adding Event: ' + JSON.stringify(event));
+    res.send(JSON.stringify({id: 'to be done', message: "ok"}));
+});
+
+
 
 var server = app.listen(9010, function() {
     console.log('Listening on port %d', server.address().port);
