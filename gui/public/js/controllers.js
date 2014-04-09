38e128939de796ab63eb540bb1a2bbfef4c02653
ArekCzarnik <czarnik@itemis.de>
2014-04-09 10:01:04 +0200

    frist gui

--- gui/public/js/controllers.js
+++ gui/public/js/controllers.js
@@ -3,16 +3,25 @@
 /* Controllers */
 
 function AppCtrl($scope, $http) {
-  $http({method: 'GET', url: '/api/name'}).
-  success(function(data, status, headers, config) {
-    $scope.name = data.name;
-  }).
-  error(function(data, status, headers, config) {
-    $scope.name = 'Error!'
-  });
+    $http({method: 'GET', url: '/api/name'}).
+        success(function (data, status, headers, config) {
+            $scope.name = data.name;
+        }).
+        error(function (data, status, headers, config) {
+            $scope.name = 'Error!'
+        });
+
+    $http({method: 'GET', url: '/api/shop'}).
+        success(function (data, status, headers, config) {
+            $scope.shop = data;
+        }).
+        error(function (data, status, headers, config) {
+            $scope.shop = 'Error!'
+        });
 }
 
-function MyCtrl1() {}
+function MyCtrl1() {
+}
 MyCtrl1.$inject = [];
 
 
